c0d4746d110221cbc7a5e89433d58c8b4d099c41
miwy <michal.wyszynski@mobica.com>
2017-12-06 14:59:47 +0100

    Function example.

--- Function/function.go
+++ Function/function.go
@@ -6,18 +6,21 @@ func add(x int, y int) int {
 	return x + y
 }
 
-//if function arguments are the same type you can omit type from all but the last.
+// If function arguments are the same type you can omit type from all but the last.
 func multiply(x, y int) int {
 	return x * y
 }
 
+// Function can return multiple results.
 func swap(x, y string) (string, string) {
 	return y, x
 }
 
 func main() {
-	a, b := swap("Hello", "world")
 	fmt.Println("28 + 83 =", add(28, 83))
+
 	fmt.Println("8 * 7 =", multiply(8, 7))
+
+	a, b := swap("Hello", "world")
 	fmt.Println("Hello World after swap: ", a, b)
 }
