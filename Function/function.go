d1b4c4f20a3bdb9b5a02f14360dd5dc0ed7dd840
miwy <michal.wyszynski@mobica.com>
2017-12-06 14:59:08 +0100

    Function example.

--- Function/function.go
+++ Function/function.go
@@ -11,7 +11,13 @@ func multiply(x, y int) int {
 	return x * y
 }
 
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
+	a, b := swap("Hello", "world")
 	fmt.Println("28 + 83 =", add(28, 83))
 	fmt.Println("8 * 7 =", multiply(8, 7))
+	fmt.Println("Hello World after swap: ", a, b)
 }
