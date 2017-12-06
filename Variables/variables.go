96c5b00ef0cf8119c51ebc40305373272871579b
miwy <michal.wyszynski@mobica.com>
2017-12-06 15:39:09 +0100

    Variables example.

--- Variables/variables.go
+++ Variables/variables.go
@@ -7,7 +7,9 @@ var c, python, java bool
 
 func main() {
 	var i int
+	// var declaration can include initializers, one per variable.
 	var text, number, logic = "Happy", 42, true
+	// var a, b = 10 <- Error: missing initializer for b.
 	fmt.Println(i, c, python, java)
 	fmt.Println(text, number, logic)
 }
